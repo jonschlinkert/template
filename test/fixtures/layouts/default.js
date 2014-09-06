5acf31226d68711693677e6469d989da39d79227
jonschlinkert <dev@sellside.com>
2014-09-06 17:54:51 -0400

    adds test fixtures

---
+++ test/fixtures/layouts/default.js
@@ -0,0 +1,12 @@
+module.exports = [
+  '<!DOCTYPE html>',
+  '<html lang="en">',
+  '  <head>',
+  '    <meta charset="UTF-8">',
+  '    <title>{{title}}</title>',
+  '  </head>',
+  '  <body>',
+  '    {% body %}',
+  '  </body>',
+  '</html>'
+].join('\n')

4f1e965055d85a7b3740733692200be9595bb880
jonschlinkert <dev@sellside.com>
2014-10-22 06:08:01 -0400

    broken test

--- test/built-in.helpers.js
+++ test/built-in.helpers.js
@@ -16,57 +16,43 @@ var async = require('async');
 
 
 describe('generated helpers:', function () {
-  // describe('build-in engines:', function () {
-  //   it('should use the `partial` helper with a built-in engine.', function (done) {
-  //     template.partial('a.md', '---\nname: "AAA"\n---\n<%= name %>', {name: 'BBB'});
-  //     var file = {path: 'a.md', content: 'foo <%= partial("a.md") %> bar'};
+  describe('build-in engines:', function () {
+    it('should use the `partial` helper with a built-in engine.', function (done) {
+      template.partial('a.md', '---\nname: "AAA"\n---\n<%= name %>', {name: 'BBB'});
+      var file = {path: 'a.md', content: 'foo <%= partial("a.md") %> bar'};
 
-  //     template.render(file, function (err, content) {
-  //       if (err) console.log(err);
-  //       content.should.equal('foo BBB bar');
-  //       done();
-  //     });
-  //   });
+      template.render(file, function (err, content) {
+        if (err) console.log(err);
+        content.should.equal('foo BBB bar');
+        done();
+      });
+    });
 
-  //   it('should use the `partial` helper and locals with a built-in engine.', function (done) {
-  //     template.partial({'abc.md': {content: '---\nname: "AAA"\n---\n<%= name %>', name: 'BBB'}});
-  //     var obj = {path: 'xyz.md', content: 'foo <%= partial("abc.md", {name: "CCC"}) %> bar'};
+    it('should use the `partial` helper and locals with a built-in engine.', function (done) {
+      template.partial({'abc.md': {content: '---\nname: "AAA"\n---\n<%= name %>', name: 'BBB'}});
+      var obj = {path: 'xyz.md', content: 'foo <%= partial("abc.md", {name: "CCC"}) %> bar'};
 
-  //     template.render(obj, {name: 'DDD'}, function (err, content) {
-  //       if (err) console.log(err);
-  //       content.should.equal('foo CCC bar');
-  //       done();
-  //     });
-  //   });
-  // });
+      template.render(obj, {name: 'DDD'}, function (err, content) {
+        if (err) console.log(err);
+        content.should.equal('foo CCC bar');
+        done();
+      });
+    });
+  });
 
 
   describe('user-defined engines:', function () {
-    it.only('should use the `partial` helper with handlebars.', function (done) {
+    it('should use the `partial` helper with handlebars.', function (done) {
       template.engine('hbs', consolidate.handlebars);
 
       template.partial('title', '<title>{{name}}</title>', {name: 'BBB'});
       template.page('a.hbs', {path: 'a.hbs', content: 'foo {{{partial "title" name}}} bar', name: 'Halle Nicole'});
 
-      template.render('a.hbs', {name: 'Halle Nicole' }, function (err, content) {
+      template.render('a.hbs', function (err, content) {
         if (err) console.log(err);
-
-        content.should.equal('foo <title>{{name}}</title> bar');
+        content.should.equal('foo <title>Halle Nicole</title> bar');
         done();
       });
-
-      // template.render('with-partial.hbs', {custom: {locals: {name: 'Halle Nicole' }}}, function (err, content) {
-      //   if (err) console.log(err);
-      //   content.should.equal('<title>Halle Nicole</title>');
-      // });
-
-      // var page = template.cache.pages[file];
-
-      // template.render(page, {custom: {locals: {name: 'Halle Nicole' }}}, function (err, content) {
-      //   if (err) return next(err);
-      //   content.should.equal('<title>Halle Nicole</title>');
-      //   done();
-      // });
     });
 
     // it('should use the `partial` helper with any engine.', function (done) {

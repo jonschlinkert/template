0d684d677c3e7cf5bc86b80a6c081ddb07a81003
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-16 12:04:46 -0500

    coverage
    adds a task for tests/coverage. updates docs

--- verbfile.js
+++ verbfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 var verb = require('verb');
 
 verb.task('readme', function() {
@@ -10,6 +12,18 @@ verb.task('readme', function() {
 verb.task('api', function() {
   verb.src('docs/.verb/*.md')
     .pipe(verb.dest('docs'));
+});
+
+verb.task('test', function (cb) {
+  verb.src('lib/**/*.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      verb.src('test/*.js')
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
 });
 
 verb.task('default', ['readme', 'api']);

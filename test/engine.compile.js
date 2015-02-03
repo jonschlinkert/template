834b1aadc233ddaef0854ec89a4ca8c2f86cc84f
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-02 19:27:13 -0500

    adds engine.compile tests

---
+++ test/engine.compile.js
@@ -0,0 +1,28 @@
+/*!
+ * template <https://github.com/jonschlinkert/template>
+ *
+ * Copyright (c) 2014-2015, Jon Schlinkert, Brian Woodward.
+ * Licensed under the MIT License (MIT)
+ */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var should = require('should');
+var consolidate = require('consolidate');
+var Template = require('..');
+var template;
+
+
+describe('engine.compile():', function () {
+  beforeEach(function () {
+    template = new Template();
+  });
+
+  it('should compile content with an engine from [consolidate].', function () {
+    template.engine('hbs', consolidate.handlebars);
+    var hbs = template.getEngine('hbs');
+    hbs.compile('{{name}}', {name: 'Jon Schlinkert'}).should.eql('{{name}}');
+  });
+});

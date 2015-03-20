d8f102e9f716b29604db8241a3ea3e8cfd65c48d
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-20 11:30:16 -0400

    move test `app` to `test/app`

---
+++ test/app/transforms/engines.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var engine = require('engine-lodash');
+
+/**
+ * Load default engines. The default engine, [engine-lodash]
+ * will process templates in any files with the `.md` extension.
+ * To change or negate these extensions, just do:
+ *
+ * ```js
+ * engine.option('defaultExts', 'md');
+ * // or an array of extensions
+ * engine.option('defaultExts', ['hbs', 'md']);
+ * ```
+ * @name default engines
+ * @api public
+ */
+
+module.exports = function (template) {
+  if (!template.disabled('default engines')) {
+    template.engine(['*', 'md'], engine, {
+      layoutDelims: template.option('layoutDelims'),
+      destExt: template.option('destExt')
+    });
+  }
+};

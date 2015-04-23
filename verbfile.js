64f42b14b26f675df2355af85255f45eb1340a3a
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-23 04:54:37 -0400

    adds reporters to istanbul task

--- verbfile.js
+++ verbfile.js
@@ -28,34 +28,36 @@ verb.helper('home', function (name) {
 });
 
 verb.task('readme', function() {
-  return verb.src('.verb.md')
+  verb.src('.verb.md')
     .pipe(verb.dest('./'));
 });
 
 verb.task('docs', function() {
-  return verb.src('docs/_verb/*.md')
+  verb.src('docs/_verb/*.md')
     .pipe(verb.dest('docs'));
 });
 
 verb.task('lint', function () {
-  return verb.src(['index.js', 'lib/**/*.js'])
+  verb.src(['index.js', 'lib/**/*.js'])
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-verb.task('test', ['lint'], function (cb) {
+verb.task('test', function (cb) {
   verb.src(['index.js', 'lib/**/*.js'])
     .pipe(istanbul({includeUntested: true}))
     .pipe(istanbul.hookRequire())
     .on('finish', function () {
       verb.src(['test/*.js'])
         .pipe(mocha())
-        .on('error', gutil.log)
-        .pipe(istanbul.writeReports())
+        .pipe(istanbul.writeReports({
+          reporters: [ 'text' ],
+          reportOpts: {dir: 'coverage', file: 'summary.txt'}
+        }))
         .on('end', function () {
           cb();
         });
     });
 });
 
-verb.task('default', ['readme', 'test']);
+verb.task('default', ['readme', 'lint', 'test']);

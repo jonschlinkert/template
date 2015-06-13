ee500a5505a6a01ba24f6a7ae49f668656a9c1eb
jonschlinkert <jon.schlinkert@sellside.com>
2015-06-13 19:53:13 -0400

    use micromatch in globber

--- test/lib/globber.js
+++ test/lib/globber.js
@@ -3,19 +3,45 @@
 var fs = require('fs');
 var _ = require('lodash');
 var path = require('path');
-var glob = require('glob');
+var mm = require('micromatch');
+var parent = require('glob-parent');
 
-module.exports = function globber(patterns, options) {
-  var files = glob.sync(patterns, options);
-  if (files.length === 0) {
-    return next(null, null);
+module.exports = globber;
+
+function globber(pattern, options) {
+  var base = parent(pattern);
+  pattern = pattern.slice(base.length);
+  base = path.resolve(base);
+
+  if (pattern.charAt(0) === '/') {
+    pattern = pattern.slice(1);
   }
 
-  return _.reduce(files, function(acc, fp) {
+  var files = lookup(base, pattern);
+  if (files.length === 0) return [];
+
+  return files.reduce(function(acc, fp) {
     acc[path.basename(fp)] = {
       content: fs.readFileSync(fp, 'utf8'),
       path: fp
     };
     return acc;
   }, {});
-};
+}
+
+function lookup(dir, pattern, res) {
+  res = res || [];
+  var files = fs.readdirSync(dir);
+  var len = files.length;
+  while (len--) {
+    var name = files[len];
+    var fp = path.join(dir, name);
+
+    if (fs.statSync(fp).isDirectory()) {
+      lookup(fp, pattern, res);
+    } else if (mm([name, fp], pattern).length) {
+      res.push(fp);
+    }
+  }
+  return res;
+}
